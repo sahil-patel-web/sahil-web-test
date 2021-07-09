1fa8977e0e728fa6dea41e767254ef547bf66f7d
sahil <spatel16300@gmail.com>
2021-07-09 00:01:31 -0700

    format done

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,102 +39,38 @@ const HomeIndex = () => {
           </ul>
         </section>
 
-        <section id="two">
+        <section id="three">
+          <h2>Skills</h2>
+        </section>
+
+        <section id="four">
+          <h2>Projects</h2>
+        </section>
+        
+        <section id="five">
           
           <h2>Resume</h2>
 
           <iframe src="https://drive.google.com/file/d/1TpLu8d1ihMkpYxg1EclgeQc4ivsvFe10/preview" width="770" height="990"></iframe>
         </section>
 
-        <section id="three">
-          <h2>Get In Touch</h2>
-          <p>
-            Accumsan pellentesque commodo blandit enim arcu non at amet id arcu
-            magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem
-            vulputate lorem neque lorem ipsum dolor.
-          </p>
-          <div className="row">
-            <div className="8u 12u$(small)">
-              <form method="post" action="mailto:sahil.patel@columbia.edu">
-                <div className="row uniform 50%">
-                  <div className="6u 12u$(xsmall)">
-                    <input
-                      type="text"
-                      name="name"
-                      id="name"
-                      placeholder="Name"
-                    />
-                  </div>
-                  <div className="6u 12u$(xsmall)">
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div className="12u">
-                    <textarea
-                      name="message"
-                      id="message"
-                      placeholder="Message"
-                      rows="4"
-                    ></textarea>
-                  </div>
-                </div>
-                <ul className="actions" style={{ marginTop: 30 }}>
-                  <li>
-                    <input type="submit" value="Send Message" />
-                  </li>
-                </ul>
-              </form>
-            </div>
-            <div className="4u 12u$(small)">
-              <ul className="labeled-icons">
-                <li>
-                  <h3 className="icon fa-home">
-                    <span className="label">Address</span>
-                  </h3>
-                  1234 Somewhere Rd.
-                  <br />
-                  Nashville, TN 00000
-                  <br />
-                  United States
-                </li>
-                <li>
-                  <h3 className="icon fa-mobile">
-                    <span className="label">Phone</span>
-                  </h3>
-                  000-000-0000
-                </li>
-                <li>
-                  <h3 className="icon fa-envelope-o">
-                    <span className="label">Email</span>
-                  </h3>
-                  <a href="#">hello@untitled.tld</a>
-                </li>
-              </ul>
-            </div>
-          </div>
+        <section id="six">
+          <h2>Hobbies</h2>
         </section>
 
-        <section id="three">
+        <section id="seven">
           <center>
           <h2> <a href="mailto:sahil.patel@columbia.edu"> Sahil Patel </a> </h2>
-
           <ul className="copyright">
             &copy; Gatsby Starter Strata<br />
             
               <a href="https://www.gatsbyjs.com/starters/codebushi/gatsby-starter-strata">
-                  Source  
+                  Design  
               </a>
               <a href="http://html5up.net"> HTML5 UP</a>
             
           </ul>
-
-
           </center>
-          
         </section>
 
 
