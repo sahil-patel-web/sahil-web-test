ba8e79717c423d05d313be350225de60e36774fc
sahil <spatel16300@gmail.com>
2021-07-15 15:13:03 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,8 +25,7 @@ const HomeIndex = () => {
             I am currently working on . For my MSCS program, I also spend part of my 
             time at Columbia's Data Science Institute. 
             
-            Prior to Columbia, I completed my bachelor's in Computer Science at
-            TAMU with a minor in Statistics. My undergraduate thesis can be
+            Prior to Columbia, I completed my bachelor's in Computer Science. My undergraduate thesis can be
             found here.
           </p>
           <ul className="actions">
@@ -39,33 +38,32 @@ const HomeIndex = () => {
         </section>
 
         <section id="two">
-          <h2>Bio</h2>
+          <h1>Bio</h1>
           <p>
             I am a proud American and a proud Immigrant. 
-            </p>
+          </p>
           <p>
             I grew up in India but my family and I migrated to America 
             when I was 13. After 4 years, 3 states, 6 apartments and 5 high schools, we finally found financial/familial structure 
             after moving to the Greater Houston area. In 2017, I joined TAMU's College of Engineering to complete my Bachelor's in Computer Science
             and a minor in Statistics. Finally, I moved to NYC to join the MSCS program at Columbia University. 
           </p>
-
           <p>
             I enjoy running and cooking!
           </p>
         </section>
 
         <section id="three">
-          <h2>Skills</h2>
+          <h1>Skills</h1>
         </section>
 
         <section id="four">
-          <h2>Projects</h2>
+          <h1>Projects</h1>
         </section>
         
         <section id="five">
           
-          <h2>Resume</h2>
+          <h1>Resume</h1>
 
           <iframe src="https://drive.google.com/file/d/1TpLu8d1ihMkpYxg1EclgeQc4ivsvFe10/preview" width="770" height="990"></iframe>
         </section>
@@ -74,7 +72,8 @@ const HomeIndex = () => {
 
         <section id="six">
           <center>
-          <h2> <a href="mailto:spatel16300@gmail.com"> Sahil Patel </a> </h2>
+          <h2>Sahil Patel</h2>
+          <p><a href="mailto:spatel16300@gmail.com"> spatel16300@gmail.com </a> </p>
           <ul className="copyright">
             &copy; Gatsby Starter Strata<br />
             
