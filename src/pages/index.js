0da324e7495f025fec1519beb94d099645fa8952
sahil <spatel16300@gmail.com>
2021-07-15 18:21:02 -0700

    p spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,9 +17,9 @@ const HomeIndex = () => {
       <div id="main">
         <section id="one">
           <header className="major">
-            <h1>
+            <h2>
               I am a Back-end software, Data Science and ML enthusiast.
-            </h1>
+            </h2>
           </header>
           <p>
             I am currently working on . For my MSCS program, I also spend part of my 
@@ -39,18 +39,18 @@ const HomeIndex = () => {
 
         <section id="two">
           <h2>Bio</h2>
-          <h5>
+          <p>
             I am a proud American and a proud Immigrant. 
-          </h5>
-          <h5>
+          </p>
+          <p>
             I grew up in India but my family and I migrated to America 
             when I was 13. After 4 years, 3 states, 6 apartments and 5 high schools, we finally found financial/familial structure 
             after moving to the Greater Houston area. In 2017, I joined TAMU's College of Engineering to complete my Bachelor's in Computer Science
             and a minor in Statistics. Finally, I moved to NYC to join the MSCS program at Columbia University. 
-          </h5>
-          <h5>
+          </p>
+          <p>
             I enjoy running and cooking!
-          </h5>
+          </p>
         </section>
 
         <section id="three">
