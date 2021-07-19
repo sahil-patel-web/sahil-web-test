24dd67befee19351e9f9238b2d7259b29915f8b5
sahil <spatel16300@gmail.com>
2021-07-19 15:23:53 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
+import pic_12 from '../assets/images/pic_12.jpg'
 
 const HomeIndex = () => {
   const siteTitle = 'Sahil Patel'
