97fd02c5ffe121d60d71a4e41f5bcb1c1888c22d
sahil <spatel16300@gmail.com>
2021-07-19 22:54:44 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,7 +73,7 @@ const HomeIndex = () => {
             <div class="content">
                 <p>hi</p>
                 <div className="content">
-                  <img src={pic_12} />
+                  <img src={col} style="width:100%;" />
                   <h3>Hello</h3>
                   <p>Now..</p>
                 </div>
