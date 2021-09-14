0e53cbd7d9f8c562b5138b08a9bdcda7e3ff26f8
sahil <spatel16300@gmail.com>
2021-09-14 12:46:29 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const HomeIndex = () => {
       <div id="main">
         <section id="one">
             <h2>
-              I am a Back-end software, Data Science and ML enthusiast.
+              I am a Back-end software, UI and Data Science enthusiast.
             </h2>
 
           <p>
